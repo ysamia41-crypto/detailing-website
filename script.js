2b2c6f67e38536c4d346771edbe6d6b7e6e285ba
ysamia41-crypto <ysamia41@gmail.com>
2026-06-24 18:26:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const menuBtn = document.getElementById("menuBtn");
+const nav = document.getElementById("nav");
 
+menuBtn.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+const sections = document.querySelectorAll(
+  ".section, .intro, .addons, .why, .cta"
+);
+
+sections.forEach((section) => {
+  section.classList.add("reveal");
+});
+
+window.addEventListener("scroll", () => {
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const screenPosition = window.innerHeight - 100;
+
+    if (sectionTop < screenPosition) {
+      section.classList.add("active");
+    }
+  });
+});
